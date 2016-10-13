bedc05aa2b65d3fbe825dcc13dab5df4d4117df8
Geno Roupsky <geno@roupsky.name>
2016-10-13 20:48:42 +0300

    support next/prev using left/right and page up/down keys

--- app/scripts/directives/quotes.js
+++ app/scripts/directives/quotes.js
@@ -2,16 +2,22 @@
  * Created by groupsky on 26.04.16.
  */
 
-require('../main').directive('quotes', /*@ngInject*/function ($animate, $compile, $templateCache, $timeout) {
+require('../main').directive('quotes', /*@ngInject*/function ($animate, $compile, $rootElement, $templateCache, $timeout) {
     var tmpl = $templateCache.get('/quote.html');
     return function ($scope, $element, $attrs) {
         var changePeriod = $attrs.quotes || 15;
+        var elements = [];
+        var timeoutCb = false;
+        var idx = -1;
+        var manual = false;
 
         var currentElement = $compile(tmpl)($scope);
         $element.append(currentElement);
 
-        function next() {
-            var nextElement = $compile(tmpl)($scope);
+        function show() {
+            $timeout.cancel(timeoutCb);
+
+            var nextElement = elements[idx];
             $element.append(nextElement);
 
             $animate.enter(nextElement, $element, currentElement);
@@ -19,9 +25,45 @@ require('../main').directive('quotes', /*@ngInject*/function ($animate, $compile
 
             currentElement = nextElement;
 
-            $timeout(next, changePeriod * 1000);
+            timeoutCb = $timeout(next, (manual ? 3 : 1) * changePeriod * 1000);
+            manual = false;
+        }
+
+        function prev() {
+            if (idx == 0) return;
+            idx--;
+            show();
+        }
+
+        function next() {
+            idx++;
+            if (idx >= elements.length) {
+                elements.push($compile(tmpl)($scope));
+            }
+            show();
         }
 
         next();
+
+        $rootElement.on('keydown', function(event) {
+            console.log(event, event.keyCode);
+            switch (event.keyCode) {
+                // page up
+                case 33:
+                // left arrow
+                case 37:
+                    manual = true;
+                    $timeout(prev);
+                    break;
+                // page down
+                case 34:
+                // right arrow
+                case 39:
+                    manual = true;
+                    $timeout(next);
+                    break;
+            }
+        })
+
     };
 });
